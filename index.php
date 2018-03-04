aa574609727e71b5161d4b4e04fecb86afc9538e
Andryz980 <andryboe@gmail.com>
2018-03-04 21:48:02 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php
+
+    $isnewlogin = true;
+    $friendname = "shy";
+
+    if (isset($_COOKIE['vusername']))
+    {
+        $isnewlogin = false;
+        $friendname = $_COOKIE['vusername'];
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html>
+   
+    <head>
+        <title>My Comfy Website</title>
+        
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- Implementato correttamente Bootstrap -->
+
+        <link rel="icon" href="pics/vicon.png">
+    </head>
+
+    <body style="background-color:#c7b3ff"> <!-- edit -->
+        
+        <div class="container">
+            
+            <div class="col-xs-12" style="height:20px;"></div> <!-- Space -->            
+            
+            <div class="col-xs-12">
+                <h1>Welcome to a sad website 🌈</h1>
+            </div>
+            
+            <?php
+            
+            if ($friendname == "shy")
+                echo '
+            <div class="col-xs-12" align="center">
+                <h3>Wanna be friends?</h3>
+            
+                <h5>(I mean if you don\'t that\'s fine too)</h5>
+            </div>
+            
+            <div class="col-xs-12" style="height:20px;"></div> <!-- Space -->            
+            
+            <form method="get" action="tables.php">';
+            else
+                echo '';
+            ?>
+            
+                <div class="col-xs-4" align="right">
+                    <img src="pics/kirby.gif" class="img-circle" style="width:100px;height:100px;">
+                </div>
+                
+            <?php
+            
+            if ($friendname == "shy")
+                echo '
+                
+                <div class="col-xs-4" align="center">
+                    <div class="form-group">
+                        <label>Your name:</label>
+                        <input type="text" class="form-control" name="newfriend">
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-default">Join</button>
+                    </div>
+                </div>
+                ';
+            else
+                echo '<div class="col-xs-4" align="center">
+                        <h4>Wait... ' . $friendname . '? You came back despite this website being so awful..?</h4><br>
+                        <h4>I mean, thanks! I really appreciate it haha wow</h4></div>';
+            ?>
+                <div class="col-xs-4">
+                    <img src="pics/kirby.gif" class="img-circle" style="width:100px;height:100px;">
+                </div>
+                
+            <?php
+            
+            if ($friendname == "shy")
+                echo '</form>'; 
+            
+            ?>
+            
+            <div class="col-xs-12" style="height:10px;"></div> <!-- Space -->   
+            
+            <div class="col-xs-12">
+                
+                <h3>Enjoy some nice pics anyway</h3>
+                
+                <div class="col-xs-12" style="height:20px;"></div> <!-- Space -->   
+                
+                <div class="col-xs-4">
+                    <img src="pics/lights.png" class="img-rounded" style="width:350px;height:200px;">
+                </div>
+                <div class="col-xs-4">
+                    <img src="pics/way.png" class="img-rounded" style="width:350px;height:200px;">
+                </div>
+                <div class="col-xs-4">
+                    <img src="pics/rain.gif" class="img-rounded" style="width:350px;height:200px;">
+                </div>
+
+                
+            </div>
+            
+        </div>
+        
+    </body>
+    
+</html>
---
+++ tables.php
@@ -0,0 +1,107 @@
+<?php
+
+    $friendname = $_GET['newfriend'];
+    if ($friendname == '')
+        $friendname = "Shy friend";
+    
+    $cuser = setcookie("vusername", $friendname, time() + (10 * 365 * 24 * 60 * 60), "/");
+
+?>
+
+<!DOCTYPE html>
+<html>
+   
+    <head>
+        <title>Tables</title>
+        
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- Implementato correttamente Bootstrap -->
+
+        <link rel="icon" href="pics/vicon.png">
+    </head>
+
+    <body style="background-color:#c7b3ff"> <!-- edit -->
+        
+        <div class="container">
+            
+            <div class="col-xs-12" style="height:20px;"></div> <!-- Space -->            
+            
+            <div class="col-xs-12"><h2>Hi new friend, join the others</h2></div>
+            
+        </div>
+        
+        <div class="col-xs-12" style="height:50px;"></div> <!-- Space -->
+        
+        <div class="col-xs-2"></div>
+        
+        <div class="col-xs-8">
+        
+            <table class="table table-hover">
+
+                <thead>
+
+                    <tr>
+                        <th>Name</th>
+                        <th>Role</th>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Me</th>
+                        <th>Haha</th>
+                    </tr>
+                    <tr>
+                        <th>You, <?php echo $friendname; ?></th>
+                        <th>My new best friend</th>
+                    </tr>
+                    <tr>
+                        <th>Cucciola2.0</th>
+                        <th>Bae</th>
+                    </tr>
+                    <tr>
+                        <th>Idk</th>
+                        <th>I'm sad</th>
+                    </tr>
+                    <tr>
+                        <th>Yeah him</th>
+                        <th>lol</th>
+                    </tr>
+                </tbody>
+
+            </table>
+            
+        </div>
+            
+        <div class="col-xs-12" style="height:30px;"></div> <!-- Space -->
+            
+            
+        <div class="col-xs-2"></div>
+        
+        <div class="col-xs-8"align="center">
+            <div class="progress">
+                <div class="progress-bar progress-bar-success" role="progressbar" style="width:10%">
+                    Friends
+                </div>
+                <div class="progress-bar progress-bar-info" role="progressbar" style="width:20%">
+                    Special friends
+                </div>
+                <div class="progress-bar progress-bar-danger" role="progressbar" style="width:60%">
+                    Social anxiety
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-xs-12" style="height:30px;"></div> <!-- Space -->
+        
+        <div class="col-xs-12" align="center">
+            <button type="button" class="btn btn-info" name="wow" onclick="alert('Thanks I hope you\'re happy')">Wow very nice table good job I like it</button>
+        </div>
+        
+    </body>
+    
+</html>
